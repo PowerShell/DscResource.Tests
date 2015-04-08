d1a7a007411e028bdcc50ba644ac3c84e9f61044
PowerShellTeam <PowerShellTeam@hotmail.com>
2015-04-08 14:22:39 -0700

    Initial commit. Add meta tests and meta fixers.

---
+++ Meta.Tests.ps1
@@ -0,0 +1,54 @@
+﻿<# 
+.summary
+    Test that describes code.
+#>
+
+[CmdletBinding()]
+param()
+
+if (!$PSScriptRoot) # $PSScriptRoot is not defined in 2.0
+{
+    $PSScriptRoot = [System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Path)
+}
+
+$ErrorActionPreference = 'stop'
+Set-StrictMode -Version latest
+
+$RepoRoot = (Resolve-Path $PSScriptRoot\..).Path
+
+Import-Module $PSScriptRoot\Fixers.psm1
+
+Describe 'Text files formatting' {
+    
+    $allTextFiles = Get-TextFilesList $RepoRoot
+    
+    Context 'Files encoding' {
+
+        It "Doesn't use Unicode encoding" {
+            $unicodeFilesCount = 0
+            $allTextFiles | %{
+                if (Test-FileUnicode $_) {
+                    $unicodeFilesCount += 1
+                    Write-Warning "File $($_.FullName) contains 0x00 bytes. It's probably uses Unicode and need to be converted to UTF-8. Use Fixer 'Get-UnicodeFilesList `$pwd | ConvertTo-UTF8'."
+                }
+            }
+            $unicodeFilesCount | Should Be 0
+        }
+    }
+
+    Context 'Indentations' {
+
+        It "Uses spaces for indentation, not tabs" {
+            $totalTabsCount = 0
+            $allTextFiles | %{
+                $fileName = $_.FullName
+                $tabStrings = (cat $_.FullName -Raw) | Select-String "`t" | % {
+                    Write-Warning "There are tab in $fileName. Use Fixer 'Get-TextFilesList `$pwd | ConvertTo-SpaceIndentation'."
+                    $totalTabsCount++
+                }
+            }
+            $totalTabsCount | Should Be 0
+        }
+    }
+}
+
---
+++ MetaFixers.psm1
@@ -0,0 +1,82 @@
+﻿<#
+    This module helps fix problems, found by Meta.Tests.ps1
+#>
+
+$ErrorActionPreference = 'stop'
+Set-StrictMode -Version latest
+
+function ConvertTo-UTF8()
+{
+    [CmdletBinding()]
+    [OutputType([void])]
+    param(
+        [Parameter(ValueFromPipeline=$true, Mandatory=$true)]
+        [System.IO.FileInfo]$fileInfo
+    )
+
+    process 
+    {
+        $content = Get-Content -Raw -Encoding Unicode -Path $fileInfo.FullName
+        Set-Content -Encoding UTF8 -Value $content -Path $fileInfo.FullName
+    }
+}
+
+function ConvertTo-SpaceIndentation()
+{
+    [CmdletBinding()]
+    [OutputType([void])]
+    param(
+        [Parameter(ValueFromPipeline=$true, Mandatory=$true)]
+        [System.IO.FileInfo]$fileInfo
+    )
+
+    process 
+    {
+        $content = (Get-Content -Raw -Path $fileInfo.FullName) -replace "`t",'    '
+        Set-Content -Value $content -Path $fileInfo.FullName
+    }
+}
+
+function Get-TextFilesList
+{
+    [CmdletBinding()]
+    [OutputType([System.IO.FileInfo])]
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$root
+    )
+    ls -File -Recurse $root | ? { @('.gitignore', '.gitattributes', '.ps1', '.psm1', '.psd1', '.json', '.xml', '.cmd', '.mof') -contains $_.Extension } 
+}
+
+function Test-FileUnicode
+{
+    
+    [CmdletBinding()]
+    [OutputType([bool])]
+    param(
+        [Parameter(ValueFromPipeline=$true, Mandatory=$true)]
+        [System.IO.FileInfo]$fileInfo
+    )
+
+    process 
+    {
+
+        $path = $fileInfo.FullName
+        $bytes = [System.IO.File]::ReadAllBytes($path)
+        $zeroBytes = @($bytes -eq 0)
+        return [bool]$zeroBytes.Length
+
+    }
+}
+
+function Get-UnicodeFilesList()
+{
+    [CmdletBinding()]
+    [OutputType([System.IO.FileInfo])]
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$root
+    )
+
+    Get-TextFilesList $root | ? { Test-FileUnicode $_ }
+}
