3af2f2e1bee29b6f36c2d048b2b17e7d0a514e9a
Karol Kaczmarek <KarolKaczmarek@users.noreply.github.com>
2015-09-08 14:46:36 -0700

    Adding function for running tests for all cloned modules

---
+++ TestRunner.psm1
@@ -0,0 +1,25 @@
+﻿<#
+    .SYNOPSIS Runs tests on all DSC resources in given folder (including common tests)
+    .PARAM
+        resourcesPath Path where DSC resource modules have been cloned (tests will run for all modules in that path)
+    .EXAMPLE
+        $localPath = Get-GitHubRepository -repositoryUrl https://github.com/PowerShell/xWebDeploy.git -destinationPath C:\git\GitHubPowerShell -branch 'dev'
+#>
+function Start-DscResourceTests
+{
+    param(
+    [String] $resourcesPath
+    )
+    
+    $testsPath = $pwd
+    cd $resourcesPath
+    ls | % {
+        $module = $_.Name
+        Write-Host "Copying common tests from $testsPath to $resourcesPath\$module" -ForegroundColor Yellow
+        Copy-Item $testsPath "$resourcesPath\$module" -recurse -force 
+        cd $module
+        Write-Host "Running tests for $module" -ForegroundColor Yellow
+        Invoke-Pester
+        cd ..
+    }
+}
