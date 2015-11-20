e15037cfd7a680b6be7f9d0debc35a4f7d5f63ff
Daniel Scott-Raynsford <plagueho@gmail.com>
2015-11-20 16:19:52 +1300

    Test Templates added.

---
+++ Templates/integration_config_template.ps1
@@ -0,0 +1,24 @@
+<#
+.Synopsis
+   DSC Configuration Template for DSC Resource Integration tests.
+.DESCRIPTION
+   To Use:
+     1. Copy to \Tests\Integration\ folder and rename MSFT_x<ResourceName>.config.ps1 (e.g. MSFT_xFirewall.config.ps1)
+     2. Customize TODO sections.
+
+.NOTES
+#>
+
+
+# TODO: Modify ResourceName
+configuration 'MSFT_<xResourceName>' {
+    Import-DscResource -ModuleName xNetworking
+    node localhost {
+       # TODO: Modify ResourceName
+       '<xResourceName>' Integration_Test {
+            # TODO: Fill Configuration Code Here
+       }
+    }
+}
+
+# TODO: (Optional): Add More Configuration Templates
