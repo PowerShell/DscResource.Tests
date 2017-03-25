5f5cadbeac353118a1f12bcd202a85e4cacce624
Daniel Scott-Raynsford <dscottraynsford@outlook.com>
2017-03-25 13:10:54 +1300

    Added ability for DscResource.DocumentationHelper\WikiPages.psm1 to display Array type parameters correctly

--- DscResource.DocumentationHelper/WikiPages.psm1
+++ DscResource.DocumentationHelper/WikiPages.psm1
@@ -64,8 +64,16 @@ function New-DscResourceWikiSite
             $null = $output.AppendLine('| --- | --- | --- | --- | --- |')
             foreach ($property in $result.Attributes)
             {
-                $null = $output.Append("| **$($property.Name)** | $($property.State) | " + `
-                            "$($property.DataType) | $($property.Description) | ")
+                # If the attribute is an array, add [] to the DataType string
+                $dataType = $property.DataType
+                if ($property.IsArray)
+                {
+                    $dataType += '[]'
+                }
+                $null = $output.Append("| **$($property.Name)** " + `
+                    " | $($property.State) " + `
+                    " | $dataType " + `
+                    " | $($property.Description) | ")
                 if ([string]::IsNullOrEmpty($property.ValueMap) -ne $true)
                 {
                     $null = $output.Append(($property.ValueMap -Join ", "))
