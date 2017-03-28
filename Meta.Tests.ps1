2dd43b4c464feea50370ea7485f61abb3b63ac5c
Brian Farnhill <brfarn@microsoft.com>
2017-03-28 12:38:44 +1100

    Code review feedback again

--- Meta.Tests.ps1
+++ Meta.Tests.ps1
@@ -446,10 +446,13 @@ Describe 'Common Tests - Validate Example Files' -Tag 'Examples' {
         {
             $powershellModulePath = Join-Path -Path (Join-Path -Path $env:SystemRoot -ChildPath 'System32\WindowsPowerShell\v1.0\Modules') -ChildPath $repoName
 
-            Get-ChildItem -Path $env:APPVEYOR_BUILD_FOLDER -Recurse | Where-Object -FilterScript {
+            $items =  Get-ChildItem -Path $env:APPVEYOR_BUILD_FOLDER -Recurse | Where-Object -FilterScript {
                 $_.FullName -notmatch "node_modules"
-            } | Copy-Item -Destination {
-                Join-Path -Path $powershellModulePath -ChildPath $_.FullName.Substring($env:APPVEYOR_BUILD_FOLDER.length)
+            } 
+            
+            $items | Copy-Item -Destination {
+                Join-Path -Path $powershellModulePath `
+                          -ChildPath $_.FullName.Substring($env:APPVEYOR_BUILD_FOLDER.length)
             }
         }
 
