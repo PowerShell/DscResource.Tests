81e84c4d2f33643debe4d65f96f6c791690429e9
Simon Heather <simonheather99@gmail.com>
2019-06-13 01:15:48 +0100

    Update New-DscResourcePowerShellHelp Function

--- DscResource.DocumentationHelper/PowerShellHelp.psm1
+++ DscResource.DocumentationHelper/PowerShellHelp.psm1
@@ -1,30 +1,54 @@
 <#
+    Define enumeration for use by help example generation to determine the type of
+    block that a text line is within.
+#>
+if (-not ([System.Management.Automation.PSTypeName]'HelpExampleBlockType').Type)
+{
+    $typeDefinition = @'
+    public enum HelpExampleBlockType
+    {
+        None,
+        PSScriptInfo,
+        Configuration,
+        ExampleCommentHeader
+    }
+'@
+    Add-Type -TypeDefinition $typeDefinition
+}
+
+$projectRootPath = Split-Path -Path $PSScriptRoot -Parent
+$testHelperPath = Join-Path -Path $projectRootPath -ChildPath 'TestHelper.psm1'
+Import-Module -Name $testHelperPath -Force
+
+$moduleName = $ExecutionContext.SessionState.Module
+$script:localizedData = Get-LocalizedData -ModuleName $moduleName -ModuleRoot $PSScriptRoot
+
+<#
 .SYNOPSIS
 
-New-DscResourcePowerShellHelp generates PowerShell compatable help files for a DSC
+New-DscResourcePowerShellHelp generates PowerShell compatible help files for a DSC
 resource module
 
 .DESCRIPTION
 
 The New-DscResourcePowerShellHelp cmdlet will review all of the MOF based resources
 in a specified module directory and will inject PowerShell help files for each resource
-in to the specified directory. These help files include details on the property types for
+in to the resource's subdirectory. These help files include details on the property types for
 each resource, as well as a text description and examples where they exist.
-
-.PARAMETER OutputPath
-
-Where should the files be saved to (ususally the "en-US" folder inside the module for US english)
+a README.md with a text description must exist in the resource's subdirectory for the
+help file to be generated.
+These help files can then be read by passing the name of the resource as a parameter to Get-Help.
 
 .PARAMETER ModulePath
 
 The path to the root of the DSC resource module (where the PSD1 file is found, not the folder for
-and individual DSC resource)
+each individual DSC resource)
 
 .EXAMPLE
 
 This example shows how to generate help for a specific module
 
-    New-DscResourcePowerShellHelp -ModulePath C:\repos\SharePointdsc -OutputPath C:\repos\SharePointDsc\en-US
+    New-DscResourcePowerShellHelp -ModulePath C:\repos\SharePointdsc
 
 #>
 function New-DscResourcePowerShellHelp
@@ -34,58 +58,55 @@ function New-DscResourcePowerShellHelp
     (
         [parameter(Mandatory = $true)]
         [System.String]
-        $OutputPath,
-
-        [parameter(Mandatory = $true)]
-        [System.String]
         $ModulePath
     )
 
-    Import-Module (Join-Path -Path $PSScriptRoot -ChildPath "MofHelper.psm1")
+    Import-Module (Join-Path -Path $PSScriptRoot -ChildPath "MofHelper.psm1") -Verbose:$false
 
     $mofSearchPath = (Join-Path -Path $ModulePath -ChildPath "\**\*.schema.mof")
     $mofSchemas = Get-ChildItem -Path $mofSearchPath -Recurse
+    Write-Verbose -Message ($script:localizedData.FoundMofFilesMessage -f $mofSchemas.Count, $ModulePath)
     $mofSchemas | ForEach-Object {
         $mofFileObject = $_
 
-        $descriptionPath = Join-Path -Path $_.DirectoryName -ChildPath "readme.md"
+        $result = (Get-MofSchemaObject -FileName $_.FullName) | Where-Object {
+            ($_.ClassName -eq $mofFileObject.Name.Replace(".schema.mof", "")) `
+                -and ($null -ne $_.FriendlyName)
+        }
+        $descriptionPath = Join-Path -Path $mofFileObject.DirectoryName -ChildPath "readme.md"
         if (Test-Path -Path $descriptionPath)
         {
-            $result = (Get-MofSchemaObject -FileName $_.FullName) | Where-Object {
-                ($_.ClassName -eq $mofFileObject.Name.Replace(".schema.mof", "")) `
-                    -and ($null -ne $_.FriendlyName)
-            }
-            Write-Verbose -Message "Generating help document for $($result.FriendlyName)"
+            Write-Verbose -Message ($script:localizedData.GenerateHelpDocumentMessage -f $result.FriendlyName)
 
-            $output = ".NAME" + [Environment]::NewLine
-            $output += "    $($result.FriendlyName)"
-            $output += [Environment]::NewLine + [Environment]::NewLine
+            $script:output = ".NAME" + [Environment]::NewLine
+            $script:output += "    $($result.FriendlyName)"
+            $script:output += [Environment]::NewLine + [Environment]::NewLine
 
             $descriptionContent = Get-Content -Path $descriptionPath -Raw
-            $descriptionContent = $descriptionContent.Replace("**Description**", ".DESCRIPTION")
             $descriptionContent = $descriptionContent -replace "\n", "`n    "
+            $descriptionContent = $descriptionContent -replace "    # Description", ".DESCRIPTION"
 
-            $output += $descriptionContent
-            $output += [Environment]::NewLine
+            $script:output += $descriptionContent
+            $script:output += [Environment]::NewLine
 
             foreach ($property in $result.Attributes)
             {
-                $output += ".PARAMETER $($property.Name)" + [Environment]::NewLine
-                $output += "    $($property.State) - $($property.DataType)"
-                $output += [Environment]::NewLine
+                $script:output += ".PARAMETER $($property.Name)" + [Environment]::NewLine
+                $script:output += "    $($property.State) - $($property.DataType)"
+                $script:output += [Environment]::NewLine
 
                 if ([string]::IsNullOrEmpty($property.ValueMap) -ne $true)
                 {
-                    $output += "    Allowed values: "
+                    $script:output += "    Allowed values: "
                     $property.ValueMap | ForEach-Object {
-                        $output += $_ + ", "
+                        $script:output += $_ + ", "
                     }
-                    $output = $output.TrimEnd(" ")
-                    $output = $output.TrimEnd(",")
-                    $output +=  [Environment]::NewLine
+                    $script:output = $script:output.TrimEnd(" ")
+                    $script:output = $script:output.TrimEnd(",")
+                    $script:output +=  [Environment]::NewLine
                 }
-                $output += "    " + $property.Description
-                $output += [Environment]::NewLine + [Environment]::NewLine
+                $script:output += "    " + $property.Description
+                $script:output += [Environment]::NewLine + [Environment]::NewLine
             }
 
             $exampleSearchPath = "\Examples\Resources\$($result.FriendlyName)\*.ps1"
@@ -94,21 +115,174 @@ function New-DscResourcePowerShellHelp
 
             if ($null -ne $exampleFiles)
             {
+                $exampleCount = 1
+
+                Write-Verbose -Message "Found $($exampleFiles.count) Examples for resource $($result.FriendlyName)"
                 foreach ($exampleFile in $exampleFiles)
                 {
-                    $exampleContent = Get-Content -Path $exampleFile.FullName -Raw
-                    $exampleContent = $exampleContent -replace "<#"
-                    $exampleContent = $exampleContent -replace "#>"
+                    $exampleContent = Get-DscResourceHelpExampleContent `
+                        -ExamplePath $exampleFile.FullName `
+                        -ExampleNumber ($exampleCount++)
 
-                    $output += $exampleContent
-                    $output += [Environment]::NewLine
+                    $script:output += $exampleContent
+                    $script:output += [Environment]::NewLine
+                }
+            }
+            else {
+                Write-Warning -Message ($script:localizedData.NoExampleFileFoundWarning -f $result.FriendlyName)
+            }
+
+            $savePath = Join-Path -Path $mofFileObject.DirectoryName -ChildPath "\en-US\about_$($result.FriendlyName).help.txt"
+            Write-Verbose -Message ($script:localizedData.OutputHelpDocumentMessage -f $savePath)
+            $script:output | Out-File -FilePath $savePath -Encoding utf8 -Force
+        }
+        else
+        {
+            Write-Warning -Message ($script:localizedData.NoDescriptionFileFoundWarning -f $result.FriendlyName)
+        }
+    }
+}
+
+<#
+    .SYNOPSIS
+        This function reads an example file from a resource and converts
+        it to help text for inclusion in a PowerShell help file.
+
+    .DESCRIPTION
+        The function will read the example PS1 file and convert the
+        help header into the description text for the example.
+
+    .PARAMETER ExamplePath
+        The path to the example file.
+
+    .PARAMETER ModulePath
+        The number of the example.
+
+    .EXAMPLE
+        Get-DscResourceHelpExampleContent -ExamplePath 'C:\repos\NetworkingDsc\Examples\Resources\DhcpClient\1-DhcpClient_EnableDHCP.ps1' -ExampleNumber 1
+
+        Reads the content of 'C:\repos\NetworkingDsc\Examples\Resources\DhcpClient\1-DhcpClient_EnableDHCP.ps1'
+        and converts it to help text in preparation for being added to a PowerShell help file.
+#>
+
+function Get-DscResourceHelpExampleContent
+{
+    [CmdletBinding()]
+    [OutputType([System.String])]
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [System.String]
+        $ExamplePath,
+
+        [Parameter(Mandatory = $true)]
+        [System.Int32]
+        $ExampleNumber
+    )
+
+    $exampleContent = Get-Content -Path $ExamplePath
+
+    # Use a string builder to assemble the example description and code
+    $exampleDescriptionStringBuilder = New-Object -TypeName System.Text.StringBuilder
+    $exampleCodeStringBuilder = New-Object -TypeName System.Text.StringBuilder
+
+    <#
+        Step through each line in the source example and determine
+        the content and act accordingly:
+        \<#PSScriptInfo...#\> - Drop block
+        \#Requires - Drop Line
+        \<#...#\> - Drop .EXAMPLE, .SYNOPSIS and .DESCRIPTION but include all other lines
+        Configuration ... - Include entire block until EOF
+    #>
+    $blockType = [WikiExampleBlockType]::None
+
+    foreach ($exampleLine in $exampleContent)
+    {
+        Write-Debug -Message ('Processing Line: {0}' -f $exampleLine)
+
+        # Determine the behavior based on the current block type
+        switch ($blockType.ToString())
+        {
+            'PSScriptInfo'
+            {
+                Write-Debug -Message 'PSScriptInfo Block Processing'
+
+                # Exclude PSScriptInfo block from any output
+                if ($exampleLine -eq '#>')
+                {
+                    Write-Debug -Message 'PSScriptInfo Block Ended'
+
+                    # End of the PSScriptInfo block
+                    $blockType = [WikiExampleBlockType]::None
                 }
             }
 
-            $savePath = Join-Path -Path $OutputPath -ChildPath "about_$($result.FriendlyName).help.txt"
-            $output | Out-File -FilePath $savePath -Encoding utf8 -Force
+            'Configuration'
+            {
+                Write-Debug -Message 'Configuration Block Processing'
+
+                # Include all lines in the configuration block in the code output
+                $null = $exampleCodeStringBuilder.AppendLine($exampleLine)
+            }
+
+            'ExampleCommentHeader'
+            {
+                Write-Debug -Message 'ExampleCommentHeader Block Processing'
+
+                # Include all lines in Example Comment Header block except for headers
+                $exampleLine = $exampleLine.TrimStart()
+
+                if ($exampleLine -notin ('.SYNOPSIS', '.DESCRIPTION', '.EXAMPLE', '#>'))
+                {
+                    # Not a header so add this to the output
+                    $null = $exampleDescriptionStringBuilder.AppendLine($exampleLine)
+                }
+
+                if ($exampleLine -eq '#>')
+                {
+                    Write-Debug -Message 'ExampleCommentHeader Block Ended'
+
+                    # End of the Example Comment Header block
+                    $blockType = [WikiExampleBlockType]::None
+                }
+            }
+
+            default
+            {
+                Write-Debug -Message 'Not Currently Processing Block'
+
+                # Check the current line
+                if ($exampleLine.TrimStart() -eq  '<#PSScriptInfo')
+                {
+                    Write-Debug -Message 'PSScriptInfo Block Started'
+
+                    $blockType = [WikiExampleBlockType]::PSScriptInfo
+                }
+                elseif ($exampleLine -match 'Configuration')
+                {
+                    Write-Debug -Message 'Configuration Block Started'
+
+                    $null = $exampleCodeStringBuilder.AppendLine($exampleLine)
+                    $blockType = [WikiExampleBlockType]::Configuration
+                }
+                elseif ($exampleLine.TrimStart() -eq '<#')
+                {
+                    Write-Debug -Message 'ExampleCommentHeader Block Started'
+
+                    $blockType = [WikiExampleBlockType]::ExampleCommentHeader
+                }
+            }
         }
     }
+
+    # Assemble the final output
+    $null = $exampleStringBuilder = New-Object -TypeName System.Text.StringBuilder
+    $null = $exampleStringBuilder.AppendLine(".Example $ExampleNumber")
+    $null = $exampleStringBuilder.AppendLine()
+    $null = $exampleStringBuilder.AppendLine($exampleDescriptionStringBuilder)
+    $null = $exampleStringBuilder.Append($exampleCodeStringBuilder)
+
+    return $exampleStringBuilder.ToString()
 }
 
 Export-ModuleMember -Function *
---
+++ DscResource.DocumentationHelper/en-US/PowerShellHelp.strings.psd1
@@ -0,0 +1,8 @@
+# culture="en-US"
+ConvertFrom-StringData @'
+    FoundMofFilesMessage          = Found {0} MOF files in path '{1}'.
+    GenerateHelpDocumentMessage   = Generating help document for '{0}'.
+    OutputHelpDocumentMessage     = Outputting help document to '{0}'.
+    NoDescriptionFileFoundWarning = No README.md description file found for '{0}', skipping.
+    NoExampleFileFoundWarning     = No Example files found for resource '{0}'.
+'@
